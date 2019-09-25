71bdcdd804f0258bed527908805b47c67284ef84
Mikhail Bugakov <Mikhail.Bugakov@alpari.org>
2019-09-25 14:56:59 +0300

    delete setInterval

--- index.js
+++ index.js
@@ -1,86 +1,97 @@
 var promiseCounter = 0;
-var noop = function() {};
+var noop = function () {
+};
 
-Promise2 = function(executor) {
-    this.id = ++promiseCounter;
-    this.executer = executor;
-    var STATUS = {
-        PENDING: 0,
-        RESOLVED: 1,
-        REJECTED: 2
-    };
+Promise2 = function (executor) {
+  this.id = ++promiseCounter;
+  this.executer = executor;
+  this.handled = false;
+  var STATUS = {
+    PENDING: 0,
+    RESOLVED: 1,
+    REJECTED: 2
+  };
 
-    this.resolve = function(value) {
-        return new this.constructor(function(resolve, reject) {resolve(value)})
-    };
-    this.reject = function (value) {
-        return new this.constructor(function(resolve, reject) {reject(value)})
-    };
-    this.checkIsPending = function() {return this.status === STATUS.PENDING};
+  // this.constructor.prototype.resolve = function (value) {
+  //   return new this.constructor(function (resolve, reject) {
+  //     resolve(value)
+  //   })
+  // };
+  // this.constructor.prototype.reject = function (value) {
+  //   return new this.constructor(function (resolve, reject) {
+  //     reject(value)
+  //   })
+  // };
+  this.checkIsPending = function () {
+    return this.status === STATUS.PENDING
+  };
 
-    this.interval = undefined;
-    this.then = function(onFulfilled, onRejected) {
-        var pr = new this.constructor(noop);
-        this.interval = setInterval(function() {
-            if (this.status !== STATUS.PENDING) {
-                clearInterval(this.interval);
-                if (this.status === STATUS.RESOLVED) {
-                    console.log('onFulfilled');
-                    var onFulfilledValue = onFulfilled(this.value);
-                    if (this.constructor.prototype.isPrototypeOf(onFulfilledValue)) {
-                        onFulfilledValue.onResolved = function () {
-                            pr.status = STATUS.RESOLVED;
-                        }
-                    } else {
-                        pr.status = STATUS.RESOLVED;
-                    }
-                } else if (this.status === STATUS.REJECTED) {
-                    var pr2 = onRejected(this.value).onReject = function () {
-                        pr.status = STATUS.REJECTED;
-                    };
-                }
-            }
-        }.bind(this), 5);
-        return pr;
-    };
-    this.status = STATUS.PENDING;
-    this.value = undefined;
-
-    this.onResolved = noop;
-    this.onReject = noop;
-
-    var resolve = function(value) {
-        this.value = value;
-        this.status = STATUS.RESOLVED;
-        this.onResolved();
+  this.interval = undefined;
+  this.then = function (onFulfilled, onRejected) {
+    var pr = new this.constructor(noop);
+    this.onHandle = function () {
+      if (this.status === STATUS.RESOLVED) {
+        var onFulfilledValue = onFulfilled(this.value);
+        if (this.constructor.prototype.isPrototypeOf(onFulfilledValue)) {
+          onFulfilledValue.onResolved = function () {
+            pr.resolve();
+          }
+        } else {
+          pr.resolve();
+        }
+      } else if (this.status === STATUS.REJECTED) {
+        var pr2 = onRejected(this.value).onReject = function () {
+          pr.status = STATUS.REJECTED;
+        };
+      }
     }.bind(this);
-    var reject = function(value) {
-        this.value = value;
-        this.status = STATUS.REJECTED;
-        this.onReject();
-    }.bind(this);
+    return pr;
+  }.bind(this);
+  this.status = STATUS.PENDING;
+  this.value = undefined;
 
-    this.executer(resolve, reject);
+  this.onResolved = noop;
+  this.onReject = noop;
+  this.onHandle = noop;
+
+  this.resolve = function (value) {
+    this.status = STATUS.RESOLVED;
+    this.onResolved();
+
+    this.value = value;
+    this.handled = true;
+    this.onHandle();
+  }.bind(this);
+  this.reject = function (value) {
+    this.status = STATUS.REJECTED;
+    this.onReject();
+
+    this.value = value;
+    this.handled = true;
+    this.onHandle();
+  }.bind(this);
+
+  this.executer(this.resolve, this.reject);
 };
 
 var counter = 0;
 var log = console.log;
 
 function makePromiseWithPromise(promise, promiseName) {
-    return function (message) {
-        return function () {
-            return new promise(function (resolve, reject) {
-                setTimeout(function () {
-                    var out = promiseName + ': ' + message + ' :' + ++counter;
-                    console.log(out);
-                    resolve(out);
-                }, 1000)
-            })
-        }
+  return function (message) {
+    return function () {
+      return new promise(function (resolve, reject) {
+        setTimeout(function () {
+          var out = promiseName + ': ' + message + ' :' + ++counter;
+          console.log(out);
+          resolve(out);
+        }, 1000)
+      })
     }
+  }
 }
 
-var makePromise  = makePromiseWithPromise(Promise, 'Promise ');
+var makePromise = makePromiseWithPromise(Promise, 'Promise ');
 var makePromise2 = makePromiseWithPromise(Promise2, 'Promise2');
 
 // console.log(new Promise(function(some, some) {}));
